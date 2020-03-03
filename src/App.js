70019af04f1aa3061bb33e98517085ba4e8ba416
Zach Willard <zacharywillard@northwesternmutual.com>
2020-03-02 20:57:57 -0600

    add increment logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
   return (
     <div className="App">
       <header className="App-header">
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        Count: {count}
       </header>
     </div>
   );
